28372e97d7d372210c6cafe79302df5e646ac8bf
SamInfine8 <sameer8creative.tech@gmail.com>
2026-01-31 23:56:04 +0530

    Both Features-> Single+Multiple Digits Prediction Works

--- utils/segment.py
+++ utils/segment.py
@@ -2,15 +2,12 @@ import cv2
 import numpy as np
 
 def segment_digits(img):
-    # Invert & threshold
-    img = cv2.bitwise_not(img)
-    _, thresh = cv2.threshold(img, 20, 255, cv2.THRESH_BINARY)
 
-    # Morphology to separate strokes
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+    # Light threshold just to find contours
+    _, thresh = cv2.threshold(
+        img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU
+    )
 
-    # Find contours
     contours, _ = cv2.findContours(
         thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
     )
@@ -24,10 +21,11 @@ def segment_digits(img):
         x, y, w, h = cv2.boundingRect(cnt)
 
         # Ignore noise
-        if w < 12 or h < 20:
+        if w < 10 or h < 20:
             continue
 
-        digit = thresh[y:y+h, x:x+w]
+        # IMPORTANT: crop from ORIGINAL grayscale image
+        digit = img[y:y+h, x:x+w]
         digits.append(digit)
 
     return digits
